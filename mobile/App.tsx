d91073ed1943234df2172b6155a8142dca9d5061
B-Schmitz <bernardoschmitz0@gmail.com>
2022-09-29 20:29:10 -0300

    update mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
+import { useRef, useEffect } from "react";
 import { StatusBar } from "react-native";
-
 import {
   useFonts,
   Inter_400Regular,
@@ -8,10 +8,13 @@ import {
   Inter_900Black,
 } from "@expo-google-fonts/inter";
 import { Background } from "./src/components/Background";
-
+import * as Notifications from "expo-notifications";
 import { Loading } from "./src/components/Loading";
 import React from "react";
 import { Routes } from "./src/routes";
+import { Subscription } from "expo-modules-core";
+import "./src/services/notificationConfigs";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,6 +24,35 @@ export default function App() {
     Inter_900Black,
   });
 
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {});
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {});
+
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+      }
+    };
+  }, []);
+
   return (
     <Background>
       <StatusBar
@@ -29,7 +61,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <Routes/> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
